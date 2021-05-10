1a1b9daaff0f73e3248fe64e35a511c7934545dd
SergiyStepanchuk <prochliste@gmail.com>
2021-05-10 12:44:24 +0300

    init

---
+++ OOP_Lab_2.4/UnitTest/UnitTest.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Program/Matrix.h"
+#include "../Program/Matrix.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest24
+{
+	TEST_CLASS(UnitTest24)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			Matr A(3, 4);
+			Assert::AreEqual(A.GetC(), 4);
+		}
+	};
+}
